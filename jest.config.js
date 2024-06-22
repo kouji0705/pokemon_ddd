6cf0590c6ba08b74a84cfeb0d90716f586606a1d
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-06-22 17:16:53 +0900

    add:

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	moduleNameMapper: {
-		"^@entities/(.*)$": "<rootDir>/src/entities/$1",
-		"^@enums/(.*)$": "<rootDir>/src/enums/$1",
-	},
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^@enums/(.*)$': '<rootDir>/src/enums/$1'
+  }
 };
