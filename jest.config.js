01ab24c62f265c37cc0427a5247a0fc85d1684ce
koji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-06-23 14:51:06 +0900

    fix: (#4)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
-    '^@enums/(.*)$': '<rootDir>/src/enums/$1'
-  }
+	preset: "ts-jest",
+	testEnvironment: "node",
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+		"^@entities/(.*)$": "<rootDir>/src/entities/$1",
+		"^@enums/(.*)$": "<rootDir>/src/enums/$1",
+		"^@services/(.*)$": "<rootDir>/src/services/$1",
+	},
 };
